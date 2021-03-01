5139a235b4c58c46486772e4d833e69638ccdeee
ignaciomartind <ignaciomartinduci@gmail.com>
2021-02-28 21:29:53 -0300

    changes in loading badge

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import logo from '../media/SSlogoenglishsvg.svg'
+import logo from '../media/logo.svg'
+import logoEng from '../media/SSlogoenglishsvg.svg'
 
 import './styles/loading.css'
 
 function Loading(){
     return <div className="loading">
-        <div className="loading-img-container">
-            <img src={logo} alt="Shyamaswara logo" />
-        </div>
+                    <div className="loading-img-container">
+                        <img src={logo} />
+                        <img src={logoEng} alt="Shyamaswara logo" />
+                    </div>
     </div>
 }
 export default Loading
